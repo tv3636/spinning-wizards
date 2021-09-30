f3f7d8191e135436cdfef1d77899a54b1da9a48e
tv3636 <tv3636@gmail.com>
2021-09-29 19:35:27 -0700

    Code cleanup

--- spin.py
+++ spin.py
@@ -1,38 +1,40 @@
 import requests, zipfile, io, os, sys
 from PIL import Image
 
-if len(sys.argv) > 1:
-	ADDRESS = sys.argv[1]
+CONTRACT = "0x521f9c7505005cfa19a8e5786a9c3c9c9f5e6f42"
+OPENSEA_API = "https://api.opensea.io/api/v1/assets"
+WIZARD_API = "https://www.forgottenrunes.com/api/art/wizards/%d.zip"
+PAGE_SIZE = 50
 
-	CONTRACT = "0x521f9c7505005cfa19a8e5786a9c3c9c9f5e6f42"
-	OPENSEA_API = "https://api.opensea.io/api/v1/assets"
-	PAGE_SIZE = 50
-
-	print('Scouring the blockchain for wizards...')
-
+def spin(address):
 	tokens = []
-	response = {'assets': ['']}
+	response = ['']
 	querystring = {
-		"owner": ADDRESS,
+		"owner": address,
 		"asset_contract_address": CONTRACT,
 		"order_direction": "desc",
 		"offset": "0",
 		"limit": PAGE_SIZE
 	}
 
-	while response['assets']:
-		response = requests.request("GET", OPENSEA_API, params=querystring).json()
-		for wizard in response['assets']:
+	print('Scouring the blockchain for wizards...')
+
+	while response:
+		response = requests.request("GET", OPENSEA_API, params=querystring).json()['assets']
+
+		for wizard in response:
 			tokens.append(int(wizard['token_id']))
 
 		querystring['offset'] = str(int(querystring['offset']) + PAGE_SIZE)
 
 	print('Downloading turnarounds...')
-	os.mkdir('./output')
+	
+	if not os.path.exists('./output'):
+		os.mkdir('./output')
 
 	for token in tokens:
 		try:
-			r = requests.get("https://www.forgottenrunes.com/api/art/wizards/%d.zip" % token)
+			r = requests.get(WIZARD_API % token)
 			z = zipfile.ZipFile(io.BytesIO(r.content))
 			z.extractall("./downloads/%d" % token)
 
@@ -48,6 +50,7 @@ if len(sys.argv) > 1:
 		except Exception as e:
 			print("Error downloading turnarounds for wizard", token, '-', e)
 
+if len(sys.argv) > 1:
+	spin(sys.argv[1])
 else:
 	print("Please provide your wallet address. Usage: python spin.py [WALLET_ADDRESS]")
-	
